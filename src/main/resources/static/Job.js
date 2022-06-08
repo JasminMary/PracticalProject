ba195dd9db7058f4bd0d61dcc6d1c22363b38284
Jasmin <jwillis2@qa.com>
2022-06-08 16:26:01 +0100

    Fulfilled user story FSPP. Added read by functionality to jobs front-end

--- src/main/resources/static/Job.js
+++ src/main/resources/static/Job.js
@@ -40,7 +40,7 @@ function readAll() {
 
             for (let i = 0; i < data.length; i++) {
                 const chara =JSON.stringify(data[i].character)
-                showRecord(data[i].id, data[i].jobName, data[i].jobLevel, chara);
+                showAll(data[i].id, data[i].jobName, data[i].jobLevel, chara);
               }
         })
     })
@@ -48,7 +48,7 @@ function readAll() {
 }
 //if time create the table in html instead 
 //variable names are just acroynms of element type and parameter 
-function showRecord(id, jobName, jobLevel, characterId) {
+function showAll(id, jobName, jobLevel, characterId) {
     let thid = document.createElement("th")
     let thjn = document.createElement("th")
     let thjl = document.createElement("th")
@@ -85,3 +85,59 @@ function showRecord(id, jobName, jobLevel, characterId) {
 }
 
 showAllJobButton.onclick = () => readAll()
+
+let readJobsByIdTable = document.createElement("table");
+
+function readBy(id) {
+    let readJobsId = fetch(`http://localhost:8080/job/read/${id}`);
+    readJobsId.then(function(response) {
+        let job = response.json();
+        job.then(function(data){
+          allJobsTable.innerHTML = ""
+          allJobsTable.style = "width 80%";
+            document.body.appendChild(readJobsByIdTable);
+                const chara =JSON.stringify(data.character)
+                showReadBy(data.id, data.jobName, data.jobLevel, chara);
+              
+        })
+    })
+}
+//this is repetative - try to refactor if time
+function showReadBy(id, jobName, jobLevel, characterId) {
+    let thid = document.createElement("th")
+    let thjn = document.createElement("th")
+    let thjl = document.createElement("th")
+    let thci = document.createElement("th")
+
+    thid.innerHTML = "|id "
+    thjn.innerHTML = "|Job "
+    thjl.innerHTML = "|Level "
+    thci.innerHTML = "|Character info "
+    readJobsByIdTable.appendChild(thid)
+    readJobsByIdTable.appendChild(thjn)
+    readJobsByIdTable.appendChild(thjl)
+    readJobsByIdTable.appendChild(thci)
+    
+    let trRow=document.createElement("tr")
+
+    let tdid = document.createElement("td")
+    let tdjn=document.createElement("td")
+    let tdjl=document.createElement("td")
+    let tdci=document.createElement("td")
+
+
+    tdid.innerHTML = id
+    tdjn.innerHTML = jobName
+    tdjl.innerHTML = jobLevel
+    tdci.innerText = characterId
+    trRow.appendChild(tdid)
+    trRow.appendChild(tdjn)
+    trRow.appendChild(tdjl)
+    trRow.appendChild(tdci)
+
+    readJobsByIdTable.appendChild(trRow)
+
+}
+readByIdButton.onclick = () => readBy (
+    readId.value
+  )
